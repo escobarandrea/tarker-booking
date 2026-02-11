8dbcd831e78292b009313a776bbac39082bbbeb8
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-11 17:54:23 -0600

    feat: configure DbContext

---
+++ src/Tarker.Booking.Application/Interfaces/IDatabaseService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tarker.Booking.Domain.Entities.Booking;
+using Tarker.Booking.Domain.Entities.Customer;
+using Tarker.Booking.Domain.Entities.User;
+
+namespace Tarker.Booking.Application.Interfaces
+{
+    public interface IDatabaseService
+    {
+        DbSet<UserEntity> Users { get; set; }
+        DbSet<CustomerEntity> Customers { get; set; }
+        DbSet<BookingEntity> Bookings { get; set; }
+        Task<bool> SaveAsync();
+    }
+}
---
+++ src/Tarker.Booking.Persistence/Database/DatabaseService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tarker.Booking.Application.Interfaces;
+using Tarker.Booking.Domain.Entities.Booking;
+using Tarker.Booking.Domain.Entities.Customer;
+using Tarker.Booking.Domain.Entities.User;
+using Tarker.Booking.Persistence.Configuration;
+
+namespace Tarker.Booking.Persistence.Database
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public DatabaseService(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<BookingEntity> Bookings { get; set; }
+
+        public async Task<bool> SaveAsync() => await SaveChangesAsync() > 0;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            EntityConfiguration(modelBuilder);
+        }
+
+        private void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            new UserConfiguration(modelBuilder.Entity<UserEntity>());
+            new CustomerConfiguration(modelBuilder.Entity<CustomerEntity>());
+            new BookingConfiguration(modelBuilder.Entity<BookingEntity>());
+        }
+    }
+}
