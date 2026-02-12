61efdc00c69e3568d55e33a50fa172abf877ebc3
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-11 19:01:38 -0600

    chore: set up AutoMapper (no profiles yet)

---
+++ src/Tarker.Booking.Application/Configuration/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarker.Booking.Application.Configuration
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+
+        }
+    }
+}
--- src/Tarker.Booking.Application/DependencyInjectionService.cs
+++ src/Tarker.Booking.Application/DependencyInjectionService.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Tarker.Booking.Application.Configuration;
 
 namespace Tarker.Booking.Application
 {
     public static class DependencyInjectionService
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services) => services;
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var mapper = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MapperProfile())
+            });
+
+            services.AddSingleton(mapper.CreateMapper());
+
+            return services;
+        }
     }
 }
