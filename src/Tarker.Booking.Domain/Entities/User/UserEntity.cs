f00168af55ed010de0ef509959a9ed13efa50b0f
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-11 18:19:19 -0600

    feat: add UserName property to UserEntity

--- src/Tarker.Booking.Domain/Entities/User/UserEntity.cs
+++ src/Tarker.Booking.Domain/Entities/User/UserEntity.cs
@@ -10,6 +10,7 @@ namespace Tarker.Booking.Domain.Entities.User
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public ICollection<BookingEntity> Bookings { get; set; }
     }
--- src/Tarker.Booking.Persistence/Configuration/UserConfiguration.cs
+++ src/Tarker.Booking.Persistence/Configuration/UserConfiguration.cs
@@ -13,6 +13,7 @@ namespace Tarker.Booking.Persistence.Configuration
             entityBuilder.HasKey(x => x.UserId);
             entityBuilder.Property(x => x.FirstName).IsRequired();
             entityBuilder.Property(x => x.LastName).IsRequired();
+            entityBuilder.Property(x => x.UserName).IsRequired();
             entityBuilder.Property(x => x.Password).IsRequired();
             entityBuilder.HasMany(x => x.Bookings).WithOne(x => x.User).HasForeignKey(x => x.UserId);
         }
