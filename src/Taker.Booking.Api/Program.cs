67c0753cefefff05dd02ee3e3b81dc57b7f02bdc
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-11 18:20:37 -0600

    refactor: add and comment out test endpoints

--- src/Taker.Booking.Api/Program.cs
+++ src/Taker.Booking.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Tarker.Booking.Application.Interfaces;
+using Tarker.Booking.Domain.Entities.User;
 using Tarker.Booking.Persistence.Database;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,26 @@ builder.Services.AddDbContext<DatabaseService>(options => options.UseSqlServer(b
 builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
+
+//app.MapPost("/test/add", async (IDatabaseService databaseService) =>
+//{
+//    var user = new UserEntity
+//    {
+//        FirstName = "Andrea",
+//        LastName = "Escobar",
+//        UserName = "andreaescobar",
+//        Password = "a9F3c7B1xZ"
+//    };
+//    await databaseService.Users.AddAsync(user);
+//    await databaseService.SaveAsync();
+
+//    return "Create OK";
+//});
+//app.MapGet("/test/getAll", async (IDatabaseService databaseService) =>
+//{
+//    var users = await databaseService.Users.ToListAsync();
+//    return users;
+//});
 
 // Configure the HTTP request pipeline.
 
