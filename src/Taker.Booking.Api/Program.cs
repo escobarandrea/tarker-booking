4882a5ebbd1910a2bfd881f47a8b86a739845756
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-15 15:23:55 -0600

    refactor: add and comment out test endpoints

--- src/Taker.Booking.Api/Program.cs
+++ src/Taker.Booking.Api/Program.cs
@@ -11,6 +11,43 @@ builder.Services.AddWebApi().AddCommon().AddApplication().AddExternal(builder.Co
 
 var app = builder.Build();
 
+// Test endpoints
+//app.MapPost("/test/users", async (CreateUserModel model, ICreateUserCommand service) =>
+//{
+//    var result = await service.Execute(model);
+//    return Results.Ok(result);
+//});
+
+//app.MapPut("/test/users/{userId:int}", async (int userId, UpdateUserModel model, IUpdateUserCommand service) =>
+//{
+//    var updated = await service.Execute(model);
+
+//    if (updated == null)
+//        return Results.NotFound($"User with id {userId} was not found.");
+
+//    return Results.NoContent();
+//});
+
+//app.MapDelete("test/users/{userId:int}", async (int userId, IDeleteUserCommand service) =>
+//{
+//    var deleted = await service.Execute(userId);
+
+//    if (!deleted)
+//        return Results.NotFound($"User with id {userId} was not found.");
+
+//    return Results.NoContent();
+//});
+
+//app.MapPatch("/test/users/{userId:int}/password", async (int userId, UpdateUserPasswordModel model, IUpdateUserPasswordCommand service) =>
+//{
+//    var updated = await service.Execute(model);
+
+//    if (updated == false)
+//        return Results.NotFound($"User with id {userId} was not found.");
+
+//    return Results.NoContent();
+//});
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
