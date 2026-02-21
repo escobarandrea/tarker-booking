0b1dde704b9b935d0ed8ba8d7c1054cbab26cfec
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-21 15:32:23 -0600

    refactor: change type of Type in CreateBookingModel

--- src/Tarker.Booking.Application/Database/Booking/Commands/CreateBooking/CreateBookingModel.cs
+++ src/Tarker.Booking.Application/Database/Booking/Commands/CreateBooking/CreateBookingModel.cs
@@ -4,10 +4,9 @@ namespace Tarker.Booking.Application.Database.Booking.Commands.CreateBooking
 {
     public class CreateBookingModel
     {
-        public int BookingId { get; set; }
         public DateTime RegisterDate { get; set; }
         public string Code { get; set; }
-        public BookingType Type { get; set; }
+        public string Type { get; set; }
         public int CustomerId { get; set; }
         public int UserId { get; set; }
     }
