c29fea480615eb8deb7bed74bec718a7b77009e2
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-21 16:13:55 -0600

    feat: add bases model for responses

---
+++ src/Tarker.Booking.Application/Features/ResponseApiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tarker.Booking.Domain.Models;
+
+namespace Tarker.Booking.Application.Features
+{
+    public static class ResponseApiService
+    {
+        public static BaseResponseModel Response(int statusCode, object? data = null, string? message = null) =>
+        new()
+        {
+            StatusCode = statusCode,
+            Success = statusCode is >= 200 and < 300,
+            Message = message,
+            Data = data
+        };
+    }
+}
