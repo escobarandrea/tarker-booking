1b1364551f7b8b30f8aec2f2b3708d218cf35a57
Andrea Escobar <escobar_andrea@outlook.com>
2026-02-21 16:37:19 -0600

    feat: add ExceptionManager

---
+++ src/Tarker.Booking.Application/Exceptions/ExceptionManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Tarker.Booking.Application.Features;
+
+namespace Tarker.Booking.Application.Exceptions
+{
+    public class ExceptionManager : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ObjectResult(ResponseApiService.Response(statusCode: StatusCodes.Status500InternalServerError, data: null, message: context.Exception.Message));
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
